dece4daeeebef007fed28cbb731c6cf598c6bf62
Trish Griego <trish.griego@moz.com>
2017-02-22 10:36:41 -0800

    Added benchmark

---
+++ fibonacci.rb
@@ -0,0 +1,24 @@
+# fibonacci sequence
+require 'Benchmark' # Ruby Module
+
+time = Benchmark.realtime do # open Benchmark block
+  # input n
+  n = 7
+  def fibonacci(n)
+    # if n = 1, we want to output 1
+    if n == 1
+      1
+    # if n = 2, we want to output 1
+    elsif n == 2
+      1
+    # if n > 2, we want to output the sum of the previous two values
+    else
+      fibonacci(n-1) + fibonacci(n-2)
+    end
+  end
+
+# print the output of n
+puts "#{n}'s fibonacci value is #{fibonacci(n)}"
+end # close Benchmark
+
+puts "Time elapsed #{time} seconds"
